e6fcbe1381890faca180e3bb9029d23efa1fe5e7
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-09-14 16:19:49 -0600

    Welcome message

---
+++ AddressBookRestSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookRestSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address book programs using Rest Sharp");
+        }
+    }
+}
